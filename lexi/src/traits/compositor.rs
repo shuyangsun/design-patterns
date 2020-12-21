339d1508e1196d50646a8db7f2e5454e1081a88d
Shuyang Sun <sunbuffett@gmail.com>
2020-12-20 23:27:03 -0500

    Add clone to compositor trait.

--- lexi/src/traits/compositor.rs
+++ lexi/src/traits/compositor.rs
@@ -1,6 +1,6 @@
 use crate::traits::composition::Composition;
 
-pub trait Compositor {
+pub trait Compositor: Clone {
     fn set_composition(&mut self, composition: &dyn Composition);
     fn compose(&self);
 }
